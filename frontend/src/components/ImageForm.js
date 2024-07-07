223ff7df9af0269f8c009e77d99b30e40ce427bc
FemputerTech <mleicht92@gmail.com>
2024-07-07 15:20:18 -0700

    implemented the predict button functionality to send an image to the backend api

--- frontend/src/components/ImageForm.js
+++ frontend/src/components/ImageForm.js
@@ -1,18 +1,41 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function ImageForm() {
   const title = "Sign Language";
-  const [imageUrl, setImageUrl] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [image, setImage] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [confidence, setConfidence] = useState(null);
 
   // Function to handle image selection
   const handleImageSelection = (event) => {
+    let file = event.target.files[0];
     let reader = new FileReader();
     reader.onload = () => {
-      setImageUrl(reader.result);
+      setImage(reader.result);
     };
-    reader.readAsDataURL(event.target.files[0]);
+    setSelectedFile(file);
+    reader.readAsDataURL(file);
   };
 
+  // Function to handle predict button
+  const sendImage = async () => {
+    let formData = new FormData();
+    formData.append("file", selectedFile);
+
+    let response = await axios({
+      method: "post",
+      url: "http://localhost:8000/predict",
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log("Response:", response.data);
+    setPrediction(response.data.class);
+    setConfidence(response.data.confidence);
+  };
   return (
     <div className="image-form">
       <h1>{title}</h1>
@@ -20,19 +43,21 @@ function ImageForm() {
         <img
           className="selected-image"
           id="selected-image"
-          src={imageUrl}
+          src={image}
           alt="Selected"
         />
       </div>
       <input id="image-selector" type="file" onChange={handleImageSelection} />
-      <button id="predict-button">Predict</button>
+      <button id="predict-button" onClick={sendImage}>
+        Predict
+      </button>
       <p>Prediction</p>
       <p>
-        <span id="prediction"></span>
+        <span id="prediction">{prediction}</span>
       </p>
       <p>Confidence</p>
       <p>
-        <span id="confidence"></span>
+        <span id="confidence">{confidence}</span>
       </p>
     </div>
   );
