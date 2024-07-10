42823e91429741a66ad3b9d136a6f91301394522
FemputerTech <mleicht92@gmail.com>
2024-07-10 14:41:41 -0700

    added a header title

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,7 @@ import React from "react";
 import "../styles/Header.css";
 
 function Header() {
-  return (
-    <div id="header">
-      <h1>Header</h1>
-    </div>
-  );
+  return <div id="header">Header</div>;
 }
 
 export default Header;
