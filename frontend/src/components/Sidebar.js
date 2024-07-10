2ce0f084250b9a04e1452d52f100c7893c02db35
FemputerTech <mleicht92@gmail.com>
2024-07-10 08:10:45 -0700

    was using class instead of className

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,47 +8,43 @@ function Sidebar() {
     <div id="sidebar">
       <div id="logo">Logo</div>
       <div id="favorites">
-        <div class="sidebar-section">Favorites</div>
-        <ul id="favorites-list" class="sidebar-list">
-          {Favorites.map(
-            (value, key) => {
-              return (
-                <li
-                  className="item"
-                  id={window.location.pathname === value.link ? "active" : ""}
-                  key={key}
-                  onClick={() => {
-                    window.location.pathname = value.link;
-                  }}
-                >
-                  <div className="sidebar-icon">{value.icon}</div>
-                  <div className="sidebar-title">{value.title}</div>
-                </li>
-              );
-            }
-          )}
+        <div className="sidebar-section">Favorites</div>
+        <ul id="favorites-list" className="sidebar-list">
+          {Favorites.map((value, key) => {
+            return (
+              <li
+                className="item"
+                id={window.location.pathname === value.link ? "active" : ""}
+                key={key}
+                onClick={() => {
+                  window.location.pathname = value.link;
+                }}
+              >
+                <div className="sidebar-icon">{value.icon}</div>
+                <div className="sidebar-title">{value.title}</div>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div id="categories">
-        <div class="sidebar-section">Categories</div>
-        <ul id="categories-list" class="sidebar-list">
-          {Categories.map(
-            (value, key) => {
-              return (
-                <li
-                  className="item"
-                  id={window.location.pathname === value.link ? "active" : ""}
-                  key={key}
-                  onClick={() => {
-                    window.location.pathname = value.link;
-                  }}
-                >
-                  <div className="sidebar-icon">{value.icon}</div>
-                  <div className="sidebar-title">{value.title}</div>
-                </li>
-              );
-            }
-          )}
+        <div className="sidebar-section">Categories</div>
+        <ul id="categories-list" className="sidebar-list">
+          {Categories.map((value, key) => {
+            return (
+              <li
+                className="item"
+                id={window.location.pathname === value.link ? "active" : ""}
+                key={key}
+                onClick={() => {
+                  window.location.pathname = value.link;
+                }}
+              >
+                <div className="sidebar-icon">{value.icon}</div>
+                <div className="sidebar-title">{value.title}</div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
