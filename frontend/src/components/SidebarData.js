a41ca3b0a1da7f628afa78e983f878d074dbbe71
FemputerTech <mleicht92@gmail.com>
2024-07-10 12:25:25 -0700

    updated the icons

--- frontend/src/components/SidebarData.js
+++ frontend/src/components/SidebarData.js
@@ -1,29 +1,29 @@
-// import AIcon from "../assets/icons/A.svg";
-// import BIcon from "../assets/icons/B.png";
-// import CIcon from "../assets/icons/C.png";
-// import DIcon from "../assets/icons/D.png";
-// import EIcon from "../assets/icons/E.png";
-// import FIcon from "../assets/icons/F.png";
-// import GIcon from "../assets/icons/G.png";
-// import HIcon from "../assets/icons/H.png";
-// import IIcon from "../assets/icons/I.png";
-// import JIcon from "../assets/icons/J.png";
-// import KIcon from "../assets/icons/K.png";
-// import LIcon from "../assets/icons/L.png";
-// import MIcon from "../assets/icons/M.png";
-// import NIcon from "../assets/icons/N.png";
-// import OIcon from "../assets/icons/O.png";
-// import PIcon from "../assets/icons/P.png";
-// import QIcon from "../assets/icons/Q.png";
-// import RIcon from "../assets/icons/R.png";
-// import SIcon from "../assets/icons/S.png";
-// import TIcon from "../assets/icons/T.png";
-// import UIcon from "../assets/icons/U.png";
-// import VIcon from "../assets/icons/V.png";
-// import WIcon from "../assets/icons/W.png";
-// import XIcon from "../assets/icons/X.png";
-// import YIcon from "../assets/icons/Y.png";
-// import ZIcon from "../assets/icons/Z.png";
+import AIcon from "../assets/icons/A.svg";
+import BIcon from "../assets/icons/B.svg";
+import CIcon from "../assets/icons/C.svg";
+import DIcon from "../assets/icons/D.svg";
+import EIcon from "../assets/icons/E.svg";
+import FIcon from "../assets/icons/F.svg";
+import GIcon from "../assets/icons/G.svg";
+import HIcon from "../assets/icons/H.svg";
+import IIcon from "../assets/icons/I.svg";
+import JIcon from "../assets/icons/J.svg";
+import KIcon from "../assets/icons/K.svg";
+import LIcon from "../assets/icons/L.svg";
+import MIcon from "../assets/icons/M.svg";
+import NIcon from "../assets/icons/N.svg";
+import OIcon from "../assets/icons/O.svg";
+import PIcon from "../assets/icons/P.svg";
+import QIcon from "../assets/icons/Q.svg";
+import RIcon from "../assets/icons/R.svg";
+import SIcon from "../assets/icons/S.svg";
+import TIcon from "../assets/icons/T.svg";
+import UIcon from "../assets/icons/U.svg";
+import VIcon from "../assets/icons/V.svg";
+import WIcon from "../assets/icons/W.svg";
+import XIcon from "../assets/icons/X.svg";
+import YIcon from "../assets/icons/Y.svg";
+import ZIcon from "../assets/icons/Z.svg";
 
 export const Favorites = [
   {
@@ -46,133 +46,133 @@ export const Favorites = [
 export const Categories = [
   {
     title: "A",
-    // icon: <img src={AIcon} alt="A" style={{ width: "24px", height: "24" }} />,
+    icon: <img src={AIcon} alt="A" style={{ width: "48px", height: "48px" }} />,
     link: "/a",
   },
   {
     title: "B",
-    // icon: <img src={BIcon} alt="B" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={BIcon} alt="B" style={{ width: "48px", height: "48px" }} />,
     link: "/b",
   },
   {
     title: "C",
-    // icon: <img src={CIcon} alt="C" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={CIcon} alt="C" style={{ width: "48px", height: "48px" }} />,
     link: "/c",
   },
   {
     title: "D",
-    // icon: <img src={DIcon} alt="D" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={DIcon} alt="D" style={{ width: "48px", height: "48px" }} />,
     link: "/d",
   },
   {
     title: "E",
-    // icon: <img src={EIcon} alt="E" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={EIcon} alt="E" style={{ width: "48px", height: "48px" }} />,
     link: "/e",
   },
   {
     title: "F",
-    // icon: <img src={FIcon} alt="F" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={FIcon} alt="F" style={{ width: "48px", height: "48px" }} />,
     link: "/f",
     section: "main",
   },
   {
     title: "G",
-    // icon: <img src={GIcon} alt="G" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={GIcon} alt="G" style={{ width: "48px", height: "48px" }} />,
     link: "/g",
   },
   {
     title: "H",
-    // icon: <img src={HIcon} alt="H" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={HIcon} alt="H" style={{ width: "48px", height: "48px" }} />,
     link: "/h",
   },
   {
     title: "I",
-    // icon: <img src={IIcon} alt="I" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={IIcon} alt="I" style={{ width: "48px", height: "48px" }} />,
     link: "/i",
   },
   {
     title: "J",
-    // icon: <img src={JIcon} alt="J" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={JIcon} alt="J" style={{ width: "48px", height: "48px" }} />,
     link: "/j",
   },
   {
     title: "K",
-    // icon: <img src={KIcon} alt="K" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={KIcon} alt="K" style={{ width: "48px", height: "48px" }} />,
     link: "/k",
   },
   {
     title: "L",
-    // icon: <img src={LIcon} alt="L" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={LIcon} alt="L" style={{ width: "48px", height: "48px" }} />,
     link: "/l",
   },
   {
     title: "M",
-    // icon: <img src={MIcon} alt="M" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={MIcon} alt="M" style={{ width: "48px", height: "48px" }} />,
     link: "/m",
   },
   {
     title: "N",
-    // icon: <img src={NIcon} alt="N" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={NIcon} alt="N" style={{ width: "48px", height: "48px" }} />,
     link: "/n",
   },
   {
     title: "O",
-    // icon: <img src={OIcon} alt="O" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={OIcon} alt="O" style={{ width: "48px", height: "48px" }} />,
     link: "/o",
   },
   {
     title: "P",
-    // icon: <img src={PIcon} alt="P" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={PIcon} alt="P" style={{ width: "48px", height: "48px" }} />,
     link: "/p",
   },
   {
     title: "Q",
-    // icon: <img src={QIcon} alt="Q" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={QIcon} alt="Q" style={{ width: "48px", height: "48px" }} />,
     link: "/q",
   },
   {
     title: "R",
-    // icon: <img src={RIcon} alt="R" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={RIcon} alt="R" style={{ width: "48px", height: "48px" }} />,
     link: "/r",
   },
   {
     title: "S",
-    // icon: <img src={SIcon} alt="S" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={SIcon} alt="S" style={{ width: "48px", height: "48px" }} />,
     link: "/s",
   },
   {
     title: "T",
-    // icon: <img src={TIcon} alt="T" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={TIcon} alt="T" style={{ width: "48px", height: "48px" }} />,
     link: "/t",
   },
   {
     title: "U",
-    // icon: <img src={UIcon} alt="U" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={UIcon} alt="U" style={{ width: "48px", height: "48px" }} />,
     link: "/u",
   },
   {
     title: "V",
-    // icon: <img src={VIcon} alt="V" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={VIcon} alt="V" style={{ width: "48px", height: "48px" }} />,
     link: "/v",
   },
   {
     title: "W",
-    // icon: <img src={WIcon} alt="W" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={WIcon} alt="W" style={{ width: "48px", height: "48px" }} />,
     link: "/w",
   },
   {
     title: "X",
-    // icon: <img src={XIcon} alt="X" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={XIcon} alt="X" style={{ width: "48px", height: "48px" }} />,
     link: "/x",
   },
   {
     title: "Y",
-    // icon: <img src={YIcon} alt="Y" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={YIcon} alt="Y" style={{ width: "48px", height: "48px" }} />,
     link: "/y",
   },
   {
     title: "Z",
-    // icon: <img src={ZIcon} alt="Z" style={{ width: "40px", height: "40" }} />,
+    icon: <img src={ZIcon} alt="Z" style={{ width: "48px", height: "48px" }} />,
     link: "/z",
   },
 ];
