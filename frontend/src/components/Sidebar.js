bc33b8b12922454844ed07ae8d03ab63c608c587
FemputerTech <mleicht92@gmail.com>
2024-07-09 18:05:34 -0700

    removed the scrollbar for now

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -5,27 +5,50 @@ import { Scrollbars } from "react-custom-scrollbars-2";
 
 function Sidebar() {
   return (
-    <Scrollbars style={{ width: 158 }}>
-      <div className="Sidebar">
+    <div className="Sidebar">
+      <div className="favoritesSidebar">
         <ul className="SidebarList">
-          {SidebarData.map((value, key) => {
-            return (
-              <li
-                className="item"
-                id={window.location.pathname === value.link ? "active" : ""}
-                key={key}
-                onClick={() => {
-                  window.location.pathname = value.link;
-                }}
-              >
-                <div id="icon">{value.icon}</div>
-                <div id="title">{value.title}</div>
-              </li>
-            );
-          })}
+          {SidebarData.filter((value) => value.section === "favorites").map(
+            (value, key) => {
+              return (
+                <li
+                  className="item"
+                  id={window.location.pathname === value.link ? "active" : ""}
+                  key={key}
+                  onClick={() => {
+                    window.location.pathname = value.link;
+                  }}
+                >
+                  <div id="icon">{value.icon}</div>
+                  <div id="title">{value.title}</div>
+                </li>
+              );
+            }
+          )}
         </ul>
       </div>
-    </Scrollbars>
+      <div className="mainSidebar">
+        <ul className="SidebarList">
+          {SidebarData.filter((value) => value.section === "main").map(
+            (value, key) => {
+              return (
+                <li
+                  className="item"
+                  id={window.location.pathname === value.link ? "active" : ""}
+                  key={key}
+                  onClick={() => {
+                    window.location.pathname = value.link;
+                  }}
+                >
+                  <div id="icon">{value.icon}</div>
+                  <div id="title">{value.title}</div>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </div>
+    </div>
   );
 }
 
