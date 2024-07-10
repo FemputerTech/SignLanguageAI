57267709a953e2264d76b3579a8571c57502e849
FemputerTech <mleicht92@gmail.com>
2024-07-10 12:49:07 -0700

    added more adjustments to sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Sidebar.css";
 import { Categories, Favorites } from "./SidebarData";
 // import { Scrollbars } from "react-custom-scrollbars-2";
 
 function Sidebar() {
+  const [activeFavorite, setActiveFavorite] = useState("");
+  const [activeCategory, setActiveCategory] = useState("");
+
   return (
     <div id="sidebar">
       <div id="logo">Logo</div>
@@ -14,10 +17,12 @@ function Sidebar() {
             return (
               <li
                 className="item"
-                id={window.location.pathname === value.link ? "active" : ""}
+                // id={window.location.pathname === value.link ? "active" : ""}
+                id={activeFavorite === value.title ? "active" : ""}
                 key={key}
                 onClick={() => {
-                  window.location.pathname = value.link;
+                  setActiveFavorite(value.title);
+                  // window.location.pathname = value.link;
                 }}
               >
                 <div className="sidebar-icon">{value.icon}</div>
@@ -34,10 +39,12 @@ function Sidebar() {
             return (
               <li
                 className="item"
-                id={window.location.pathname === value.link ? "active" : ""}
+                // id={window.location.pathname === value.link ? "active" : ""}
+                id={activeCategory === value.title ? "active" : ""}
                 key={key}
                 onClick={() => {
-                  window.location.pathname = value.link;
+                  // window.location.pathname = value.link;
+                  setActiveCategory(value.title);
                 }}
               >
                 <div className="sidebar-icon">{value.icon}</div>
