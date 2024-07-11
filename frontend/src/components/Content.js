289e50e03fdd89726cf0ac50abcfbe0eb667462c
FemputerTech <mleicht92@gmail.com>
2024-07-11 13:18:35 -0700

    set up my http request to my google cloud function api

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../styles/Content.css";
 
 function Content() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        let response = await axios.get(
+          "https://us-west1-cloud-signlanguage-leicht.cloudfunctions.net/asl-alphabet/random"
+        );
+        console.log("image url:", response.data.url);
+        setImage(response.data.url);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
+    };
+    fetchImages();
+  }, []);
+
   return (
     <div id="content">
       <h1 id="content-title">Content</h1>
-      <div className="card-container"></div>
+      <div className="card-container">
+        <img src={image} alt="a signed letter" />
+      </div>
     </div>
   );
 }
