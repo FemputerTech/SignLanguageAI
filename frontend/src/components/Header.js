78c6dc012798a8d691ea8b0453c149471ca97e19
FemputerTech <mleicht92@gmail.com>
2024-07-11 16:52:23 -0700

    succesfully implemented the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "../styles/Header.css";
 
-function Header() {
-  return <div id="header"></div>;
+function Header({ selectedImageUrl }) {
+  const [prediction, setPrediction] = useState(null);
+  const [confidence, setConfidence] = useState(null);
+
+  const sendImage = async () => {
+    try {
+      let response = await axios.post(
+        `http://localhost:8000/predict?image_url=${encodeURIComponent(
+          selectedImageUrl
+        )}`
+      );
+      console.log("Response:", response.data);
+      setPrediction(response.data.class);
+      setConfidence(response.data.confidence);
+    } catch (error) {
+      console.error("Error sending image for prediction:", error);
+    }
+  };
+  return (
+    <div id="header">
+      <img
+        className="selected-image"
+        src={selectedImageUrl}
+        alt="Selected asl letter"
+      />
+      <button id="predict-button" onClick={() => sendImage()}>
+        Predict
+      </button>
+      <p>Prediction</p>
+      <p>
+        <span id="prediction">{prediction}</span>
+      </p>
+      <p>Confidence</p>
+      <p>
+        <span id="confidence">{confidence}</span>
+      </p>
+    </div>
+  );
 }
 
 export default Header;
