19eaf4eb8a0545dacd81e04c9043d8499f9f25ac
FemputerTech <mleicht92@gmail.com>
2024-07-11 16:21:00 -0700

    changed my predict function to accept an image url instead of a file

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, Query, HTTPException, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import tensorflow as tf
 from PIL import Image
 from io import BytesIO
 import numpy as np
 import uvicorn
+import requests
 import os
 
 app = FastAPI()
@@ -17,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
@@ -39,12 +40,25 @@ def preprocess_image(image, target_size):
     
     return processed_image
 
+# Function to fetch image from URL
+def fetch_image_from_url(image_url):
+    try:
+        response = requests.get(image_url)
+        response.raise_for_status()  # Raise error for non-successful status codes
+        image = Image.open(BytesIO(response.content))
+        return image
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Failed to fetch image from URL: {str(e)}")
+
 
 # API POST endpoint
 @app.post("/predict")
-async def predict(file: UploadFile=File(...)):
-    print("File received...")
-    image = Image.open(BytesIO(await file.read()))
+async def predict(image_url: str):
+    print("URL received:", image_url)
+    # Fetch image from URL
+    image = fetch_image_from_url(image_url)
+    
+    # process the image
     processed_image = preprocess_image(image, target_size=(256, 256))
     processed_image = np.array(processed_image)
     img_batch = np.expand_dims(processed_image, 0)
@@ -58,6 +72,23 @@ async def predict(file: UploadFile=File(...)):
         'class': predicted_class,
         'confidence': confidence
     }
+
+# async def predict(file: UploadFile=File(...)):
+#     print("File received...")
+#     image = Image.open(BytesIO(await file.read()))
+#     processed_image = preprocess_image(image, target_size=(256, 256))
+#     processed_image = np.array(processed_image)
+#     img_batch = np.expand_dims(processed_image, 0)
+    
+#     predictions = MODEL.predict(img_batch)
+
+#     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+#     confidence = float(np.max(predictions[0]))
+
+#     return {
+#         'class': predicted_class,
+#         'confidence': confidence
+#     }
 
 
 # Run the application
