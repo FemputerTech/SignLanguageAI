7cef5869b7f1350b76ea478569f5374a9cdf6ee3
FemputerTech <mleicht92@gmail.com>
2024-07-12 11:55:37 -0700

    renamed header to preview

---
+++ frontend/src/components/Preview.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/Preview.css";
+
+function Preview({ selectedImageUrl, setSelectedImageUrl, selectedFavorite }) {
+  const [prediction, setPrediction] = useState(null);
+  const [confidence, setConfidence] = useState(null);
+  useEffect(() => {
+    const fetchImage = async () => {
+      if (selectedFavorite) {
+        try {
+          let response = await axios.get(
+            `https://us-west1-cloud-signlanguage-leicht.cloudfunctions.net/asl-alphabet/${selectedFavorite}`
+          );
+          setSelectedImageUrl(response.data.url);
+        } catch (error) {
+          console.error("Error fetching images:", error);
+        }
+      }
+    };
+    fetchImage();
+  }, [selectedFavorite, setSelectedImageUrl]);
+
+  const sendImage = async () => {
+    try {
+      let response = await axios.post(
+        `http://localhost:8000/predict?image_url=${encodeURIComponent(
+          selectedImageUrl
+        )}`
+      );
+      console.log("Response:", response.data);
+      setPrediction(response.data.class);
+      setConfidence(response.data.confidence);
+    } catch (error) {
+      console.error("Error sending image for prediction:", error);
+    }
+  };
+  return (
+    <div id="preview">
+      <img
+        className="selected-image"
+        src={selectedImageUrl}
+        alt="Selected asl letter"
+      />
+      <button id="predict-button" onClick={() => sendImage()}>
+        Predict
+      </button>
+      <p>Prediction</p>
+      <p>
+        <span id="prediction">{prediction}</span>
+      </p>
+      <p>Confidence</p>
+      <p>
+        <span id="confidence">{confidence}</span>
+      </p>
+    </div>
+  );
+}
+
+export default Preview;
